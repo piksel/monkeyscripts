c55577f9992df1555df7edff2fb436da9176fae8
nils måsén <nils@piksel.se>
2021-08-08 16:27:59 +0200

    fix(prisjakt-wide-compare): CSW lookup

--- src/prisjakt-wide-compare.user.js
+++ src/prisjakt-wide-compare.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Prisjakt Wide Compare
 // @namespace    https://github.com/piksel/monkeyscripts
-// @version      0.2
+// @version      0.3
 // @description  Use all available screen space when comparing products
 // @author       nils måsen
 // @match        https://www.prisjakt.nu/jamfor/produkter/*,*
@@ -15,17 +15,19 @@
 (function() {
     'use strict';
 
-    const classStartsWith = $csw = (name) => `[class^='${name}']`;
-    const queryFromPath = (classes) => (Array.isArray(classes) ? classes : [classes]).map(classStartsWith).join(' ');
+    const queryFromPath = (classes) => (Array.isArray(classes) ? classes : [classes]).join(' ');
     const queryAll = (queries) => queries.flatMap(q => Array.from(document.querySelectorAll(q)));
     const prependParents = (qs, q, i) => Array.isArray(qs) ? [...qs, `${qs.at(-1)} ${q}`] : prependParents([qs], q, i);
+    
+    // ClassStartsWith short hand for tagged template use
+    const $csw = (name) => `[class^='${name}']`;
 
     // Apply callback to all supplied selector paths, appending the previous as a specializing selector (`foo`, `foo bar`, `foo bar baz`)
     const $qcr = (cb, ...paths) => queryAll(paths.map(queryFromPath).reduce(prependParents)).forEach(cb);
     
     // Apply callbacks to all results matching the supplied selectors
     const $qcs = (cb, ...paths) => queryAll(paths.map(queryFromPath)).forEach(cb)
-
+    
     $qcr(e => { e.style.maxWidth = '100%';   }, $csw`PageContent`, $csw`Wrapper`, $csw`Content`);
     $qcs(e => { e.style.display = 'none';    }, $csw`OutsiderAds`);
 
